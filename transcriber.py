1c022c4c4ada603c12d2aa7f130b800a3f775ce9
jgiaccio <jgiaccio@verbio.com>
2023-03-15 00:02:48 -0300

    transcriber using whisper AI

---
+++ transcriber.py
@@ -0,0 +1,27 @@
+import whisper
+
+def load_model(model):
+    
+    model_used = whisper.load_model(model)
+
+    return model_used
+
+def transcriber(model, audio_file):
+
+    audio = whisper.load_audio(audio_file)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+
+    transcription = result.text
+
+    return transcription
