76018ececa5104bc481940bf30fa5bbd94e201fb
jgiaccio <jgiaccio@verbio.com>
2023-03-15 13:56:20 -0300

    chat gpt request added

--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 import argparse
 import transcriber
 import record_audio
+import gpt_api
+import json
 
 def main(model):
 
     whisper_model = model
     loaded_model = transcriber.load_model(whisper_model)
+
     #record_audio.record_audio()
     audio = record_audio.record_audio_sr()
     record_audio.audio_to_wav(audio)
     transcription = transcriber.transcriber(loaded_model,'microphone-results.wav')
 
     print(transcription)
+    chat_answer = gpt_api.send_request(transcription)
+
+    print(chat_answer)
+    #print(str(chat_answer, 'ascii', errors='ignore'))
+    message = json.loads(str(chat_answer, 'utf8', errors='ignore'))
+    print(message)
     
 
 def parseInputArguments():
@@ -21,6 +30,8 @@ def parseInputArguments():
     parser = argparse.ArgumentParser(description='Process to create a transcription while sending the audio')
     parser.add_argument("-m","--model", default="small", help="Model to use",
                         choices=["tiny", "base", "small", "medium", "large", "large-v2"])
+    parser.add_argument("-l","--language", default="es", help="language",
+                        choices=["tiny", "base", "small", "medium", "large", "large-v2"])                    
 
     args = parser.parse_args()
     model = args.model
