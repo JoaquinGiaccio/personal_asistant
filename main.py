4ba27b22f957c1ba2431c5f63c9be0e71f69ff68
jgiaccio <jgiaccio@verbio.com>
2023-03-16 18:31:11 -0300

    ssml function formatter added

--- main.py
+++ main.py
@@ -12,26 +12,54 @@ def main(model):
 
     whisper_model = model
     loaded_model = transcriber.load_model(whisper_model)
+    print("ASR model loaded")
 
     #record_audio.record_audio()
     audio = record_audio.record_audio_sr()
+    print("Finish recording")
     record_audio.audio_to_wav(audio)
+    print("Transcribing...")
     transcription = transcriber.transcriber(loaded_model,'microphone-results.wav')
 
     print(transcription)
     chat_answer = gpt_api.send_request(transcription,gpt_model)
     chat_answer = json.loads(chat_answer)
+    print(chat_answer)
 
     print(chat_answer['choices'])
     message_dict = dict(chat_answer['choices'][0])
     print(message_dict['message']['content'])
+    gpt_message = message_dict['message']['content']
 
-    tts_status = neurasound_api.neura_speak(message_dict['message']['content'])
+    #ssml_message = format_message(message_dict['message']['content'], tts_voice, tts_accent)
+    ssml_message = format_message(gpt_message, tts_voice, tts_accent)
+    print(ssml_message)
+
+    #tts_status = neurasound_api.neura_speak(message_dict['message']['content'])
+    tts_status = neurasound_api.neura_speak(ssml_message)
 
     if tts_status != 'ERROR':
         playsound('neura_tts.wav')
     else:
         print("Error: Error in TTS message.")
+
+
+def format_message(text, voice, accent):
+
+    words = text.split()
+    formated_text = ""
+
+    for word in words:
+        x = word.isdigit()
+        if x == True:
+            ssml_tag = "<say-as interpret-as='cardinal'>" + word + "</say-as>"
+            formated_text = formated_text + " " + ssml_tag
+        else:
+            formated_text = formated_text + " " + word
+
+    formated_text = "<speak> <voice name='" + voice + "' lang='" + accent + "'>" + formated_text + "</voice> </speak>"
+
+    return formated_text
 
 
 def parseInputArguments():
@@ -41,15 +69,21 @@ def parseInputArguments():
                         choices=["tiny", "base", "small", "medium", "large", "large-v2"])
     parser.add_argument("-l","--language", default="es", help="language",
                         choices=["en", "es"])
-    parser.add_argument("-g","--gptmodel", default="gpt-3.5-turbo", help="language",
-                        choices=["gpt-3.5-turbo", "text-davinci-003"])                    
+    parser.add_argument("-g","--gptmodel", default="gpt-3.5-turbo", help="openai gpt model to be used",
+                        choices=["gpt-3.5-turbo", "text-davinci-003"])
+    parser.add_argument("--tts_voice", default="m3", help="neurasound tts voice",
+                        choices=["f1", "f2","f3", "m1", "m2", "m3"])
+    parser.add_argument("--tts_accent", default="arg", help="neurasound tts accent",
+                        choices=["arg", "col", "ven"])                     
 
     args = parser.parse_args()
     model = args.asrmodel
     gpt_model = args.gptmodel
+    tts_voice = args.tts_voice
+    tts_accent = args.tts_accent
 
-    return model, gpt_model
+    return model, gpt_model, tts_voice, tts_accent
 
 if __name__ == "__main__":
-    model, gpt_model = parseInputArguments()
+    model, gpt_model, tts_voice, tts_accent = parseInputArguments()
     main(model)
